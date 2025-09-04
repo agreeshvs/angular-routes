5c412e51c5929fba59e0f52ef1e8bcedbc77323a
agreeshvs <agreesh777@gmail.com>
2025-09-04 12:01:11 +0530

    Using Query String in Route
    Using Query String in Route

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,19 +1,48 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CourseService } from '../Services/course.service';
 import { Course } from '../Models/course';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
   coursesService = inject(CourseService);
-  AllCourses: Course[] = this.coursesService.courses;
+  AllCourses: Course[];
   router: Router = inject(Router);
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  searchString: string = '';
+
+  ngOnInit(): void {
+    // read the query parameter value from the URL
+    // this.searchString = this.activeRoute.snapshot.queryParams['search'];
+
+    // Alternative way to read query parameter value
+    /* this.searchString = this.activeRoute.snapshot.queryParamMap.get('search') || '';
+    console.log(this.searchString); */
+
+    this.activeRoute.queryParamMap.subscribe( (params) => {
+      this.searchString = params.get('search') || '';
+      console.log(this.searchString);
+      // Conditions to filter the courses.
+      if( this.searchString == undefined || this.searchString.trim() == ''){
+        this.AllCourses = this.coursesService.courses;
+      } else{
+        this.AllCourses = this.coursesService.courses.filter(
+          // filter all courses whose title contains the search string
+          course => course.title.toLowerCase().includes(
+            this.searchString.toLocaleLowerCase()
+          )
+        );
+      }
+    })
+
+    
+  }
 
   navigateToDetails(id: any){
-    this.router.navigate(['courses/course', id]);
+     this.router.navigate(['courses/course', id]);
   }
 }
