68aa7492c319f0df405ecadce4c2aefc8ce74981
agreeshvs <agreesh777@gmail.com>
2025-09-09 18:25:16 +0530

    Resolve Route Guard in Angular v14

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { IDActivateComponent } from '../Services/authguard.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements IDActivateComponent {
+  firstName: string = '';
+  lastName: string = '';
+  country: string = 'india';
+  message: string = '';
+  isSubmitted: boolean = false;
 
+
+  onSubmit(){
+    this.isSubmitted = true; 
+    alert("Form Submitted Successfully!");
+  }
+
+  canExit(): boolean{
+    if( this.firstName || this.lastName || this.message ){
+      return confirm("You have unsaved changes. Do you really want to leave?");
+    }
+    else{
+      return true;
+    }
+  }
 }
