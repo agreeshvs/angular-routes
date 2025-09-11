73c64009434e5940937eff3a375869a16c530934
agreeshvs <agreesh777@gmail.com>
2025-09-11 17:46:37 +0530

    Pure & Impure Pipe in Angular

--- src/app/Pipes/filter.pipe.ts
+++ src/app/Pipes/filter.pipe.ts
@@ -2,10 +2,12 @@ import { Pipe, PipeTransform } from "@angular/core";
 import { Student } from "../Models/Student";
 
 @Pipe({
-    name: 'filter'
+    name: 'filter', 
+    pure: false
 })
 export class FilterPipe implements PipeTransform{
     transform(list: Student[],filterBy: any){
+        console.log('FilterPipe Called')
         if( filterBy.toLowerCase() === 'all' || filterBy === '' || list.length === 0){
             return list;
         }
--- src/app/Pipes/percentage.pipe.ts
+++ src/app/Pipes/percentage.pipe.ts
@@ -6,6 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 export class PercentagePipe implements PipeTransform{
     transform(value: number, total: number, decimal: number = 0) {
+        console.log('PercentagePipe Called')
         return ((value / total) * 100).toFixed(decimal)+'%';
     }
 }
